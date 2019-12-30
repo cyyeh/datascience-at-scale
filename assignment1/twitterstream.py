11b5df140a9207b6009ef58bc1181073bf6bd16d
cyyeh <chihyu.jimmy.yeh@gmail.com>
2019-12-30 10:58:29 +0800

    finish problem1

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -1,61 +1,88 @@
 import oauth2 as oauth
 import urllib2 as urllib
+import ConfigParser
 
 # See assignment1.html instructions or README for how to get these credentials
 
-api_key = "<Enter api key>"
-api_secret = "<Enter api secret>"
-access_token_key = "<Enter your access token key here>"
-access_token_secret = "<Enter your access token secret here>"
-
 _debug = 0
-
-oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
-oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
 
 signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
 
 http_method = "GET"
 
-
-http_handler  = urllib.HTTPHandler(debuglevel=_debug)
+http_handler = urllib.HTTPHandler(debuglevel=_debug)
 https_handler = urllib.HTTPSHandler(debuglevel=_debug)
 
 '''
 Construct, sign, and open a twitter request
 using the hard-coded credentials above.
 '''
-def twitterreq(url, method, parameters):
-  req = oauth.Request.from_consumer_and_token(oauth_consumer,
-                                             token=oauth_token,
-                                             http_method=http_method,
-                                             http_url=url, 
-                                             parameters=parameters)
 
-  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
 
-  headers = req.to_header()
+def twitterreq(url, method, parameters, oauth_keys):
+    req = oauth.Request.from_consumer_and_token(oauth_keys["oauth_consumer"],
+                                                token=oauth_keys["oauth_token"],
+                                                http_method=http_method,
+                                                http_url=url,
+                                                parameters=parameters)
 
-  if http_method == "POST":
-    encoded_post_data = req.to_postdata()
-  else:
-    encoded_post_data = None
-    url = req.to_url()
+    req.sign_request(signature_method_hmac_sha1,
+                     oauth_keys["oauth_consumer"], oauth_keys["oauth_token"])
 
-  opener = urllib.OpenerDirector()
-  opener.add_handler(http_handler)
-  opener.add_handler(https_handler)
+    headers = req.to_header()
 
-  response = opener.open(url, encoded_post_data)
+    if http_method == "POST":
+        encoded_post_data = req.to_postdata()
+    else:
+        encoded_post_data = None
+        url = req.to_url()
 
-  return response
+    opener = urllib.OpenerDirector()
+    opener.add_handler(http_handler)
+    opener.add_handler(https_handler)
 
-def fetchsamples():
-  url = "https://stream.twitter.com/1/statuses/sample.json"
-  parameters = []
-  response = twitterreq(url, "GET", parameters)
-  for line in response:
-    print line.strip()
+    response = opener.open(url, encoded_post_data)
+
+    return response
+
+
+def fetchsamples(oauth_keys):
+    url = "https://stream.twitter.com/1.1/statuses/sample.json"
+    parameters = []
+    response = twitterreq(url, "GET", parameters, oauth_keys)
+    for line in response:
+        print line.strip()
+
 
 if __name__ == '__main__':
-  fetchsamples()
+    config = ConfigParser.RawConfigParser()
+    config.read('twitter_api_key.cfg')
+    api_key = config.get('twitter_api', 'api-key')
+    api_secret = config.get('twitter_api', 'api-secret')
+    access_token_key = config.get('twitter_api', 'access-token-key')
+    access_token_secret = config.get('twitter_api', 'access-token-secret')
+    if not api_key or not api_secret or not access_token_key or not access_token_secret:
+        raise Exception('twitter api key or secret is unknown')
+
+    twitter_api_keys = {
+        "api_key": api_key,
+        "api_secret": api_secret,
+        "access_token_key": access_token_key,
+        "access_token_secret": access_token_secret,
+    }
+
+    oauth_token = oauth.Token(
+        key=twitter_api_keys["access_token_key"],
+        secret=twitter_api_keys["access_token_secret"]
+    )
+    oauth_consumer = oauth.Consumer(
+        key=twitter_api_keys["api_key"],
+        secret=twitter_api_keys["api_secret"]
+    )
+
+    oauth_keys = {
+        "oauth_token": oauth_token,
+        "oauth_consumer": oauth_consumer,
+    }
+
+    fetchsamples(oauth_keys)
